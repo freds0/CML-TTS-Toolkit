b4037d0953bdd5012d3ef37c2ed1046ca564de70
Fred <fred_s0@yahoo.com.br>
2021-04-22 19:52:45 -0300

    Function Improvement
    It is now possible to continue the text conversion process, redoing sentences with low similarity.

--- execute_text_converter_on_folder.py
+++ execute_text_converter_on_folder.py
@@ -4,15 +4,15 @@ from os.path import join, dirname, isfile
 from tqdm import tqdm
 from text_converter import get_transcripts, text_cleaning, execute_threads_search_substring_by_char, execute_threads_search_substring_by_word
 
-def execute(transcript_file, complete_text_file, search_type, output_file, number_threads):
-
-    output_f = open(output_file, "w")
+def execute_first_convertion(transcript_file, complete_text_file, search_type, output_file, number_threads):
 
     with open(transcript_file) as f:
         transcripts_text = f.readlines()
 
     with open(complete_text_file) as f:
         book_text = f.read()
+
+    output_f = open(output_file, "w")
 
     # Cleaning complete text
     book_text = text_cleaning(book_text)
@@ -48,6 +48,53 @@ def execute(transcript_file, complete_text_file, search_type, output_file, numbe
 
     output_f.close()
 
+def continue_convertion(transcript_file, complete_text_file, search_type, output_file, number_threads):
+
+    with open(transcript_file) as f:
+        transcripts_text = f.readlines()
+
+    with open(complete_text_file) as f:
+        book_text = f.read()
+
+    output_f = open(output_file, "w")
+
+    # Cleaning complete text
+    book_text = text_cleaning(book_text)
+
+    start_position = 0
+    separator = '|'
+    total_similarity = 0
+
+    # Iterates over each transcription
+    for line in tqdm(transcripts_text):
+        filename, text, text_result, similarity = line.split('|')
+
+        if float(similarity) > 0.9:
+            line = separator.join([filename.strip(), text.strip(), text_result.strip(), str(similarity.strip()) + '\n'])
+            output_f.write(line)
+            continue
+
+        print('Processing {}'.format(filename))
+
+        if search_type == 'char':
+            text_result, similarity, start_position = execute_threads_search_substring_by_char(text, book_text, start_position=0, similarity_metric='hamming', total_threads=int(number_threads))
+        else:
+            text_result, similarity, start_position = execute_threads_search_substring_by_word(text, book_text, start_position=0, similarity_metric='hamming', total_threads=int(number_threads))
+
+        if not text_result:
+            text_result = ''
+        total_similarity += similarity
+
+        # Debug
+        print(text.strip())
+        print(text_result.strip())
+        print(similarity)
+        line = separator.join([filename.strip(), text.strip(), text_result.strip(), str(similarity) + '\n'])
+        output_f.write(line)
+
+    print('Mean Similarity: {}'.format(total_similarity / len(transcripts_text)))
+
+    output_f.close()
 
 def main():
     parser = argparse.ArgumentParser()
@@ -62,20 +109,23 @@ def main():
     args = parser.parse_args()
     
     for transcript_file in glob(join(args.base_dir, args.input_folder) + '/**/**/transcripts.txt'):
+
         output_folder = dirname(transcript_file)
         book_file = transcript_file.split('/')[-2]
         output_filepath = join(output_folder, 'output.txt')
         complete_text_file = join(args.base_dir, args.books_folder, book_file + '.txt')
+
         # Verify if folder alread converted
         if isfile(output_filepath):
             with open(output_filepath) as f:
                 content_output = f.readlines()
             with open(transcript_file) as f:
                 content_input = f.readlines()
+
             if len(content_output) == len(content_input):
-                print('{} já existe!'.format(output_filepath))
-                continue
-        execute(transcript_file, complete_text_file, args.search_type, output_filepath, int(args.number_threads))
+                continue_convertion(output_filepath, complete_text_file, args.search_type, output_filepath, int(args.number_threads))
+
+        execute_first_convertion(transcript_file, complete_text_file, args.search_type, output_filepath, int(args.number_threads))
 
 if __name__ == "__main__":
     main()
